4e961ff0eb67887d03300f20b5996ba8dada78c1
Anirudh Sarve <107532050+anisarve2006@users.noreply.github.com>
2025-02-04 21:06:09 +0530

    Update time_dilation_simulator.py
    Pyroid code script modified

--- time_dilation_simulator.py
+++ time_dilation_simulator.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2024  Tony Boutwell - tonyboutwell@gmail.com
-# This program is free software: you can redistribute it and/or modify it under the terms of the 
-# GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
-# or (at your option) any later version.
-
 import sys
 import numpy as np
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, 
